1913c104dc80df6e6d9149224c23d51650e143fe
Mohamed Mahmoud <132668058+Mohamed-Mahmoud98@users.noreply.github.com>
2025-05-20 00:30:07 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_ec2_instance" {
-  ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2 AMI (for us-east-1)
+  ami           = "ami-084568db4383264d4"  # Amazon Linux 2 AMI (for us-east-1)
   instance_type = "t2.micro"
   key_name      = "mykey"                  # Must match the key name in AWS
 
