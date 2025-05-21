90e6bc262cbf3c3629d6bd30a48bab76e277143d
Mohamed Mahmoud <132668058+Mohamed-Mahmoud98@users.noreply.github.com>
2025-05-21 13:19:57 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,7 +33,7 @@ data "aws_vpc" "default" {
 resource "aws_instance" "my_ec2_instance" {
   ami           = "ami-084568db4383264d4"
   instance_type = "t2.micro"
-  key_name      = "mykey"
+  key_name      = "ec2_key"
 
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
